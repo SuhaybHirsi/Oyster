3c57a16831457b83be9d7ed38db8bb25f8b9ef13
Mashkoor Ahmed <mashkoor_ahmed@hotmail.co.uk>
2017-11-22 19:37:55 +0000

    Journey End Return Tests

---
+++ src/com/tfl/tests/JourneyEndTest.java
@@ -0,0 +1,37 @@
+package com.tfl.tests;
+
+import com.tfl.billing.JourneyEvent;
+import com.tfl.billing.JourneyEnd;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class JourneyEndTest {
+    UUID CARD_ID = UUID.randomUUID();
+    UUID READER_ID = UUID.randomUUID();
+
+
+    JourneyEvent end_journey = new JourneyEnd(CARD_ID, READER_ID);
+
+    @Test
+    public void checksReturnOfCardID()
+    {
+        assertThat(end_journey.cardId(), is(CARD_ID));
+
+    }
+
+    @Test
+    public void checksReturnOfReaderID()
+    {
+        assertThat(end_journey.readerId(), is(READER_ID));
+
+    }
+
+
+
+
+}
