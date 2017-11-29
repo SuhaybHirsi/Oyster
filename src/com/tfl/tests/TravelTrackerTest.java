a3c5b85b1575598b7ffefe41a016404a71ff73e4
SuhaybHirsi <suhayb40@live.com>
2017-11-29 16:23:23 +0000

    test to ChargeCustomerTravellingFromPeakToOffPeak added

--- src/com/tfl/tests/TravelTrackerTest.java
+++ src/com/tfl/tests/TravelTrackerTest.java
@@ -1,12 +1,10 @@
 package com.tfl.tests;
 
 import com.tfl.billing.*;
-import com.tfl.external.CustomerDatabase;
+import com.tfl.underground.OysterReaderLocator;
+import com.tfl.underground.Station;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 
@@ -15,8 +13,6 @@ import java.util.UUID;
 
 import com.oyster.*;
 import com.tfl.external.Customer;
-import com.tfl.external.CustomerDatabase;
-import com.tfl.external.PaymentsSystem;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -25,24 +21,25 @@ import com.oyster.OysterCard;
 
 public class TravelTrackerTest {
 
-    private class clockTestDouble implements ClockInterface
+    private class ClockTestDouble implements ClockInterface
     {
-        ArrayList<Integer> myTimes = new ArrayList<Integer>() {{
-            add(25200000);
-            add(75200000);
-        }};
+        ArrayList<Long> myTimes = new ArrayList<>();
         @Override
         public long getTime()
         {
-            long temp = (long) myTimes.get(0);
+            long temp = myTimes.get(0);
             myTimes.remove(0);
 
             return  temp;
         }
+        public void addTime(long time)
+        {
+            myTimes.add(time);
+        }
 
     }
 
-    private class customerDatabaseTestDouble implements Database
+    private class CustomerDatabaseTestDouble implements Database
     {
         private List<Customer> customers = new ArrayList<Customer>() {
             {
@@ -50,18 +47,10 @@ public class TravelTrackerTest {
 //                this.add(new Customer("Shelly Cooper", new OysterCard("3f1b3b55-f266-4426-ba1b-bcc506541866")));
 //                this.add(new Customer("Oliver Morrell", new OysterCard("07b0bcb1-87df-447f-bf5c-d9961ab9d01e")));
 //                this.add(new Customer("Jesse Schmitz", new OysterCard("3b5a03cb-2be6-4ed3-b83e-94858b43e407")));
-//                this.add(new Customer("Jenny King", new OysterCard("89adbd1c-4de6-40e5-98bc-b3315c6873f2")));
-//                this.add(new Customer("Danny Conner", new OysterCard("609e72ac-8be3-4476-8b45-01db8c7f122b")));
-//                this.add(new Customer("Fern Taylor", new OysterCard("ffa3f53a-e225-43ea-b854-5130a1fa016d")));
-//                this.add(new Customer("Logan Wakefield", new OysterCard("cd5e097d-e2f1-4bc4-bf7b-d47903d89441")));
-//                this.add(new Customer("Angela Harris", new OysterCard("365d7f7d-0ff5-4f87-a51d-f6443a36d035")));
-//                this.add(new Customer("John Smith", new OysterCard("0c79d9e0-3874-4b02-9492-80c232a07640")));
-//                this.add(new Customer("Mary Clarke", new OysterCard("34bbfc54-916b-42b2-a6d8-5ec2eaf7dd7a")));
-//                this.add(new Customer("Emily Scott", new OysterCard("267b3378-592d-4da7-825d-3252982d49ba")));
             }
 
             public boolean add(Customer customer) {
-                return Math.random() > 0.2D ? super.add(customer) : false;
+                return super.add(customer);
             }
         };
 
@@ -77,7 +66,7 @@ public class TravelTrackerTest {
         }
     }
 
-    private class paymentSystemTestDouble implements GeneralPaymentsSystem
+    private class PaymentSystemTestDouble implements GeneralPaymentsSystem
     {
 
         @Override
@@ -95,22 +84,39 @@ public class TravelTrackerTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    ClockTestDouble myClock= new ClockTestDouble();
+    final Database myCustomerDB = new CustomerDatabaseTestDouble();
+    final GeneralPaymentsSystem myPS= new PaymentSystemTestDouble();
+
+    final OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+
+    OysterCardReader paddingtonReader = OysterReaderLocator.atStation(Station.PADDINGTON);
+    OysterCardReader bakerStreetReader = OysterReaderLocator.atStation(Station.BAKER_STREET);
+    OysterCardReader kingsCrossReader = OysterReaderLocator.atStation(Station.KINGS_CROSS);
 
 
     @Test
-    public void chargeAccounts() {
-        ClockInterface myClock= new clockTestDouble();
-        Database myCustomerDB = new customerDatabaseTestDouble();
-        GeneralPaymentsSystem myPS= new paymentSystemTestDouble();
-//
-//        new TravelTracker(myCustomerDB, myPS, myClock).chargeAccounts();
-        TravelTracker myTravelTracker = new TravelTracker(myCustomerDB, myPS, myClock);
-        UUID CARD_ID = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-        UUID START_READER_ID = UUID.randomUUID();
-        UUID END_READER_ID = UUID.randomUUID();
-        myTravelTracker.cardScanned(CARD_ID, START_READER_ID);
-        myTravelTracker.cardScanned(CARD_ID,END_READER_ID);
-        myTravelTracker.chargeAccounts();
+    public void chargeAccountsForOneTripStartingAtPeakAndEndingAtOffPeak() {
+
+
+        myClock.addTime(25200000l); //peak
+        myClock.addTime(75200000l); //off peak
+
+        TravelTracker travelTracker = new TravelTracker(myCustomerDB, myPS, myClock);
+
+        travelTracker.connect(paddingtonReader, bakerStreetReader, kingsCrossReader);
+
+        paddingtonReader.touch(myCard);
+
+        bakerStreetReader.touch(myCard);
+
+
+        travelTracker.chargeAccounts();
+
+//        UUID CARD_ID = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+//        UUID START_READER_ID = UUID.randomUUID();
+//        UUID END_READER_ID = UUID.randomUUID();
+
     }
 
     @Test
@@ -119,9 +125,7 @@ public class TravelTrackerTest {
 
     @Test
     public void cardScanned() {
-        ClockInterface myClock= new clockTestDouble();
-        Database myCustomerDB = new customerDatabaseTestDouble();
-        GeneralPaymentsSystem myPS= new paymentSystemTestDouble();
+
 
     }
 
